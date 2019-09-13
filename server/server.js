9c536754d7389c5c4a7f0a46d586a3f668d9a784
Pavel Stepin <pvstepin@avito.ru>
2019-09-14 02:16:33 +0300

    basic auth

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const errorhandler = require('errorhandler');
+const basicAuth = require('basic-auth');
 
 const db = require('./db');
 const logger = require('./logger');
@@ -20,6 +21,26 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const auth = function (req, res, next) {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.send(401);
+    }
+
+    const user = basicAuth(req);
+
+    // Если пользователь не ввёл пароль или логин, снова показать форму.
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(res);
+    }
+
+    return user.name === 'admin' && user.pass === 'brus-bany' ? next() : unauthorized(res);
+};
+
+app.use('/admin', auth, function(req, res, next) {
+    next();
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
