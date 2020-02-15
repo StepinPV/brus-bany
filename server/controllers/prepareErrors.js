5a12e14d457fd621fb7065e3608ca93f5b1bf135
Pavel Stepin <pvstepin@avito.ru>
2020-02-15 17:51:58 +0300

    Тестим ошибку

--- server/controllers/prepareErrors.js
+++ server/controllers/prepareErrors.js
@@ -1,24 +1,28 @@
 const prepareErrors = errors => {
-    const preparedErrors = {};
+    try {
+        const preparedErrors = {};
 
-    const appendError = (errors, key, error) => {
-        const separatorIndex = key.indexOf('.');
+        const appendError = (errors, key, error) => {
+            const separatorIndex = key.indexOf('.');
 
-        if (separatorIndex !== -1) {
-            const errorKey = key.substring(0, separatorIndex);
+            if (separatorIndex !== -1) {
+                const errorKey = key.substring(0, separatorIndex);
 
-            errors[errorKey] = errors[errorKey] || {};
-            appendError(errors[errorKey], key.substring(separatorIndex + 1, key.length), error);
-        } else {
-            errors[key] = error.message;
-        }
-    };
+                errors[errorKey] = errors[errorKey] || {};
+                appendError(errors[errorKey], key.substring(separatorIndex + 1, key.length), error);
+            } else {
+                errors[key] = error.message;
+            }
+        };
 
-    Object.keys(errors).forEach(key => {
-        appendError(preparedErrors, key, errors[key]);
-    });
+        Object.keys(errors).forEach(key => {
+            appendError(preparedErrors, key, errors[key]);
+        });
 
-    return preparedErrors;
+        return preparedErrors;
+    } catch(err) {
+        return JSON.stringify(errors);
+    }
 };
 
 module.exports = prepareErrors;
