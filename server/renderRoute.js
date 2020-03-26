2fabd7ef07289c00d8253626dfb3d100d89819ba
Pavel Stepin <pvstepin@avito.ru>
2020-03-27 00:45:06 +0300

    Добавил Cache-Control

--- server/renderRoute.js
+++ server/renderRoute.js
@@ -56,6 +56,7 @@ router.get('*', async (req, res, next) => {
             }
 
             res.set('Link', preloadList);
+            res.setHeader('Cache-Control', 'max-age');
 
             res.render('index.pug', {
                 isProduction: process.env.NODE_ENV === 'production',
