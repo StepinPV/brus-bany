a7318e43719421a2923bac7f3e1be3ade54cb5c3
Pavel Stepin <pvstepin@avito.ru>
2020-03-29 15:12:39 +0300

    Кнопки в галерее

--- src/client/pages/Project/resources/Gallery/Gallery.js
+++ src/client/pages/Project/resources/Gallery/Gallery.js
@@ -9,32 +9,60 @@ class Gallery extends PureComponent {
     };
 
     state = {
-        activeImageIndex: 0
+        index: 0
     };
 
     render() {
         const { images } = this.props;
-        const { activeImageIndex } = this.state;
+        const { index } = this.state;
 
-       return (
-           <>
-               <img className={styles.activeImage} src={images[activeImageIndex].src} alt={images[activeImageIndex].alt} itemProp="image" />
-               <div className={styles.images}>
-                   {images.map((image, index) => {
-                       return (
-                           <img
-                               itemProp="image"
-                               key={image.src}
-                               onClick={() => { this.setState({ activeImageIndex: index }) }}
-                               className={cx(styles.image, {[styles.selectedImage]: index === activeImageIndex})}
-                               src={image.src}
-                               alt={image.alt} />
-                       )
-                   })}
-               </div>
-           </>
-       );
+        return (
+            <>
+                <div className={styles.imageContainer}>
+                    <div className={styles.prevContainer} onClick={this.onPrev}>
+                        <i className={styles.prev} />
+                    </div>
+                    <div className={styles.nextContainer} onClick={this.onNext}>
+                        <i className={styles.next} />
+                    </div>
+                    <img className={styles.activeImage} src={images[index].src} alt={images[index].alt} itemProp="image" />
+                </div>
+                <div className={styles.images}>
+                    {images.map((image, index) => {
+                        return (
+                            <img
+                                itemProp="image"
+                                key={image.src}
+                                onClick={() => { this.setState({ index: index }) }}
+                                className={cx(styles.image, {[styles.selectedImage]: index === index})}
+                                src={image.src}
+                                alt={image.alt} />
+                        )
+                    })}
+                </div>
+            </>
+        );
     }
+
+    onPrev = () => {
+        const { images } = this.props;
+        const { index } = this.state;
+        const newIndex = index - 1;
+
+        this.setState({
+            index: newIndex === -1 ? images.length - 1 : newIndex
+        });
+    };
+
+    onNext = () => {
+        const { images } = this.props;
+        const { index } = this.state;
+        const newIndex = index + 1;
+
+        this.setState({
+            index: newIndex === images.length ? 0 : newIndex
+        });
+    };
 }
 
 export default Gallery;
