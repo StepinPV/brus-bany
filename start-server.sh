46d56950c108c06c72adaecf4687431ae7bd120c
Pavel Stepin <pvstepin@avito.ru>
2020-04-11 13:20:51 +0300

    start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,8 @@
-NODE_ENV=production node server/server.js
+#!/bin/bash
+
+# полный путь до скрипта
+ABSOLUTE_FILENAME=`readlink -e /root/brus-bany`
+# каталог в котором лежит скрипт
+DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`
+
+NODE_ENV=production node $DIRECTORY/server/server.js
