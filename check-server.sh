b61928a964e4bedb6a7bbb9041d30e49042cd77c
Pavel Stepin <pvstepin@avito.ru>
2020-04-11 14:59:25 +0300

    start-server.sh

---
+++ check-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [netstat -tulpn | grep ':3000 ']
+then
+  echo 3000
+fi
+
+if [netstat -tulpn | grep ':3001 ']
+then
+  echo 3001
+fi
--- start-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /root/brus-bany
-npm run server:production
