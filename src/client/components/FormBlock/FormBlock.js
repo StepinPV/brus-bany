eb84dc382a6019ddf56efb416ae79aea966a5055
Pavel Stepin <pvstepin@avito.ru>
2020-04-23 01:59:26 +0300

    Форма заявки

--- src/client/components/FormBlock/FormBlock.js
+++ src/client/components/FormBlock/FormBlock.js
@@ -1,31 +1,31 @@
 import React, { memo } from 'react';
 import SocialNetwork from '../../components/SocialNetwork';
-import DataSection from '../DataSection';
+import Section from '../Section';
+import Caption from '../Caption';
+import Text from '../Text';
 import Form from '../Form';
 import styles from './FormBlock.module.css';
 
 export default memo(({ source, data }) => {
     return (
-        <DataSection caption='Звоните — расскажем о банях все и поможем с выбором' id='requestForm'>
+        <Section id='requestForm'>
             <div className={styles.content}>
                 <div className={styles['first-column']}>
+                    <div>
+                        <Caption align='center' color='white'>Звоните!</Caption>
+                        <br/>
+                        <Caption size='s' align='center' color='white'>Расскажем о банях все и поможем с выбором</Caption>
+                        <br/>
+                    </div>
                     <div className={styles['phone-block']}>
                         <a href="tel:88002010729" className={styles['phone-block-phone']}>8 (800) 201-07-29</a>
                         <a href="mailto:info@brus-bany.ru" className={styles['phone-block-email']}>info@brus-bany.ru</a>
-                    </div>
-                    <div className={styles['address-block']}>174510, Новгородская обл, <br/> г. Пестово, ул. Курганная 12</div>
-                    <div className={styles['social-networks']}>
-                        <SocialNetwork type='vk' className={styles['social-network']} />
-                        <SocialNetwork type='fb' className={styles['social-network']} />
-                        <SocialNetwork type='ok' className={styles['social-network']} />
-                        <SocialNetwork type='inst' className={styles['social-network']} />
-                        <SocialNetwork type='youtube' className={styles['social-network']} />
                     </div>
                 </div>
                 <div className={styles['second-column']}>
                     <Form source={source} data={data} />
                 </div>
             </div>
-        </DataSection>
+        </Section>
     );
 });
