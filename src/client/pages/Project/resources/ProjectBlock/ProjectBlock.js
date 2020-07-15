cb6d3f571a088c17886e48fad374a2ea42f4b2c9
Pavel Stepin <pvstepin@avito.ru>
2020-07-16 00:43:10 +0300

    CardMini

--- src/client/pages/Project/resources/ProjectBlock/ProjectBlock.js
+++ src/client/pages/Project/resources/ProjectBlock/ProjectBlock.js
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import Card from '../../../../../components/Card';
+import Card from '../../../../../components/CardMini';
 import DataSection from '../../../../components/DataSection';
 import styles from './ProjectBlock.module.css';
 
