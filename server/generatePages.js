b8a9da81e54c635c59d6c7572608bf5090cecc6b
Pavel Stepin <pvstepin@avito.ru>
2021-07-09 16:59:27 +0300

    Генерация html

--- server/generatePages.js
+++ server/generatePages.js
@@ -4,54 +4,52 @@ const getPageHtml = require('./getPageHtml');
 
 const Pages = require('./controllers/Pages');
 
-let interval;
+let globalUpdatingIndex = 0;
 
 module.exports = async function () {
-    if (interval) {
-        clearInterval(interval);
-    }
+    globalUpdatingIndex = globalUpdatingIndex + 1;
 
     logger.info(`\nГенерация pages для сайта ${process.env.NAME}...`);
 
+    // pages
+    const { data: pages } = await Pages.getAll();
+
+    const generatePage = async (index, updatingIndex) => {
+        const page = pages[index];
+        const url = page.get('url');
+
+        if (url) {
+            const { html } = await getPageHtml({ path: url, url });
+
+            const name = url.split('/').pop() || 'index';
+            const path = url.split('/').splice(0, url.split('/').length - 1).join('/');
+
+            const folder = `./sites/${process.env.NAME}/pages/${path}`;
+
+            if (!fs.existsSync(folder)){
+                fs.mkdirSync(folder, { recursive: true });
+            }
+
+            fs.writeFile(`${folder}/${name}.html`, html, function (err) {
+                if (err) {
+                    logger.error(`Ошибка генерации страницы ${url} для сайта ${process.env.NAME}: ${err}`, );
+                    return;
+                }
+
+                logger.success(`Страница ${index}/${pages.length} ${url} для сайта ${process.env.NAME} сгенерирована`);
+            });
+        }
+
+        if (index + 1 !== pages.length && updatingIndex === globalUpdatingIndex) {
+            setTimeout(() => {
+                generatePage(index + 1, updatingIndex);
+            }, 2000);
+        }
+    };
+
     fs.rmdir(`./sites/${process.env.NAME}/pages/`, { recursive: true }, async () => {
-        // pages
-        const { data: pages } = await Pages.getAll();
-        let pageIndex = 0;
-
         if (pages.length) {
-            interval = setInterval(async () => {
-                const page = pages[pageIndex];
-                const url = page.get('url');
-
-                if (url) {
-                    const { html } = await getPageHtml({ path: url, url });
-
-                    const name = url.split('/').pop() || 'index';
-                    const path = url.split('/').splice(0, url.split('/').length - 1).join('/');
-
-                    const folder = `./sites/${process.env.NAME}/pages/${path}`;
-
-                    if (!fs.existsSync(folder)){
-                        fs.mkdirSync(folder, { recursive: true });
-                    }
-
-                    fs.writeFile(`${folder}/${name}.html`, html,
-                        function (err) {
-                            if (err) {
-                                logger.error(`Ошибка генерации страницы ${url} для сайта ${process.env.NAME}: ${err}`, );
-                                return;
-                            }
-
-                            logger.success(`${Math.trunc(pageIndex/pages.length * 100)}% Страница ${url} для сайта ${process.env.NAME} сгенерирована`);
-                        });
-                }
-
-                pageIndex++;
-
-                if (pageIndex >= pages.length) {
-                    clearInterval(interval);
-                }
-            }, 500);
+            await generatePage(0, globalUpdatingIndex)
         }
     });
 };
